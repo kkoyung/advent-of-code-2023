73ed3457248910efb65a6454b0b634dd1f0239ec
Kingsley Yung <kingsley@kkoyung.dev>
2023-12-11 20:33:57 +0800

    Add explanation to day06

--- day06a/src/main.rs
+++ day06a/src/main.rs
@@ -21,13 +21,20 @@ fn main() {
         .iter()
         .zip(distances.iter())
         .map(|(time, distance)| {
+            // Let t = time, d = distance, p = pressing duration.
+            // Since p*(t-p)>d, we know
+            //  upper_bound_of_press_time = (t+sqrt(t^2-4d))/2  (exclusive)
+            //  lower_bound_of_press_time = (t-sqrt(t^2-4d))/2  (exclusive)
+
             let inside_root = (time * time) - (4 * distance);
             if inside_root > 0 {
                 let root = (inside_root as f64).sqrt();
                 let upper_bound = ((*time as f64) + root) / 2.0; // exclusive
                 let lower_bound = ((*time as f64) - root) / 2.0; // exclusive
+
+                // number of valid choices
+                //  = (upper_bound.ceil() as u64 - 1) - (lower_bound.floor() as u64 + 1) + 1
                 (upper_bound.ceil() as u64) - (lower_bound.floor() as u64) - 1
-                // == (upper_bound.ceil() as u64 - 1) - (lower_bound.floor() as u64 + 1) + 1
             } else {
                 0
             }
--- day06b/src/main.rs
+++ day06b/src/main.rs
@@ -18,13 +18,21 @@ fn main() {
     let distance: u64 = numbers.next().unwrap();
 
     // Start calculation
+
+    // Let t = time, d = distance, p = pressing duration.
+    // Since p*(t-p)>d, we know
+    //  upper_bound_of_press_time = (t+sqrt(t^2-4d))/2  (exclusive)
+    //  lower_bound_of_press_time = (t-sqrt(t^2-4d))/2  (exclusive)
+
     let inside_root = (time * time) - (4 * distance);
     let result = if inside_root > 0 {
         let root = (inside_root as f64).sqrt();
         let upper_bound = ((time as f64) + root) / 2.0; // exclusive
         let lower_bound = ((time as f64) - root) / 2.0; // exclusive
+        
+        // number of valid choices
+        //  = (upper_bound.ceil() as u64 - 1) - (lower_bound.floor() as u64 + 1) + 1
         (upper_bound.ceil() as u64) - (lower_bound.floor() as u64) - 1
-        // == (upper_bound.ceil() as u64 - 1) - (lower_bound.floor() as u64 + 1) + 1
     } else {
         0
     };
